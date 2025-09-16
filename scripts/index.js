6c45171e88eab24e2cc6bfef51ebb76c9463837b
Josef D'Angelo <josefdangelo407@gmail.com>
2025-09-16 14:32:44 -0400

    Spots Project 5 Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const initialCards = [
   {
+    name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -35,6 +39,9 @@ const editProfileNameInput = editProfileModal.querySelector(
 const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
+const previewModal = document.querySelector("#preview-modal");
+const previewModalClose = previewModal.querySelector(".modal__close-btn");
+const previewModalImage = previewModal.querySelector(".modal__image");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
@@ -58,9 +65,25 @@ function getCardElement(data) {
   const cardTitleEl = cardElement.querySelector(".card__title");
   const cardImageEl = cardElement.querySelector(".card__image");
 
+  const likeBtnOriginal = cardElement.querySelector(".card__like-button");
+  likeBtnOriginal.addEventListener("click", () => {
+    likeBtnOriginal.classList.toggle("card__like-button_active");
+  });
+
+  const trashButton = cardElement.querySelector(".card__delete-button");
+  trashButton.addEventListener("click", function () {
+   cardElement.remove(".card");
+  });
+
   cardImageEl.src = data.link;
   cardImageEl.alt = data.name;
   cardTitleEl.textContent = data.name;
+
+  cardImageEl.addEventListener("click", function () {
+    previewModal.classList.add("modal_is-opened");
+    previewModalImage.src = data.link;
+    previewModalImage.alt = data.name;
+  });
 
   return cardElement;
 }
@@ -90,6 +113,10 @@ newPostBtn.addEventListener("click", function () {
 });
 newPostCloseBtn.addEventListener("click", function () {
   closeModal(newPostModal);
+});
+
+previewModalClose.addEventListener("click", function () {
+  previewModal.classList.remove("modal_is-opened");
 });
 
 function handleEditProfileSubmit(evt) {
